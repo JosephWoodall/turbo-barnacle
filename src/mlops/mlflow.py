c95dd4098a891b209389c811d5b10f11796a9ede
Joseph Woodall <josephlw4@gmail.com>
2023-01-12 10:28:37 -0600

    adds mlops component

---
+++ src/mlops/mlflow.py
@@ -0,0 +1,28 @@
+import mlflow
+import mlflow.sklearn
+import os
+
+"""
+The script defines a train_and_log_model function that takes in the training data and a machine learning model as input. It starts an MLflow run, 
+logs the model and its hyperparameters using mlflow.sklearn.log_model, logs the training data using mlflow.log_artifact, 
+logs the environment variables using mlflow.log_param and logs the accuracy of the model using mlflow.log_metric.
+
+You can extend this script to include more functionality as per your requirement like logging other metrics like precision, 
+recall or f1-score, logging model performance on test dataset, and more complex monitoring, versioning, and deployment functionality.
+
+It's important to note that mlflow is a powerful tool that allows you to track and manage your machine learning experiments 
+and models, but if you are looking for a more complete solution for MLOps, you might want to consider using other tools such as Kubeflow, 
+MLFlow or TensorFlow Extended (TFX) which provide more advanced functionality to manage machine learning models throughout the entire ML lifecycle.
+"""
+
+def train_and_log_model(data, model):
+    # Start an MLflow run
+    with mlflow.start_run():
+        # Log the model and its hyperparameters
+        mlflow.sklearn.log_model(model, "model")
+        # Log the training data
+        mlflow.log_artifact(data, "data")
+        # Log the environment variables
+        mlflow.log_param("environment", os.environ)
+        # Log the accuracy of the model
+        mlflow.log_metric("accuracy", model.score(data))
