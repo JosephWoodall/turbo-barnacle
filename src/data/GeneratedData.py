54782badb7fd25513bf263edc7f8a266231746e3
Joseph Woodall <josephlw4@gmail.com>
2023-01-12 09:09:06 -0600

    fixes Generated Data

--- src/data/GeneratedData.py
+++ src/data/GeneratedData.py
@@ -1,7 +1,25 @@
 from faker import Faker
 import uuid 
+import random
 
-class GeneratedData():
+class DataGenerator:
+    def __init__(self, num_rows, num_cols):
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.faker = Faker()
+        self.data = {}
+        
+    def generate_data(self):
+        for i in range(self.num_cols):
+            self.data[f'col{i}'] = []
+        for _ in range(self.num_rows):
+            for i in range(self.num_cols):
+                self.data[f'col{i}'].append(self.faker.random_element())
+        return self.data
+
+"""
+# The below code is deprecated, but I wanted to keep it for records anyways
+class GeneratedData:
     
     def __init__(self):
         self.data = {}
@@ -48,4 +66,4 @@ class GeneratedData():
             self.data.update(self.user)
         return self.data
         #print(self.data)
-
+"""
