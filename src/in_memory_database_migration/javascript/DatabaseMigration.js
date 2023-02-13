f7b9817b2e31093fa44e5f14af9306fbb9e83334
Joseph Woodall <josephlw4@gmail.com>
2023-02-13 10:21:40 -0600

    changes DatabaseMigration

--- src/in_memory_database_migration/javascript/DatabaseMigration.js
+++ src/in_memory_database_migration/javascript/DatabaseMigration.js
@@ -1,82 +1,83 @@
 class DatabaseMigration {
-    constructor(source_type, target_type, source_conn_params, target_conn_params) {
-      this.source_type = source_type;
-      this.target_type = target_type;
-      this.source_conn_params = source_conn_params;
-      this.target_conn_params = target_conn_params;
-    }
-  
-    fetchDataFromSource() {
-      try {
-        // Connect to source database
-        const source_conn = this.connectToSource();
-  
-        // Get cursor to source database
-        const source_cursor = source_conn.cursor();
-  
-        // Execute query to fetch data from source
-        source_cursor.execute(`SELECT * FROM source_table`);
-  
-        // Fetch all data from source database
-        let data = source_cursor.fetchall();
-  
-        // Show success message
-        console.log("Data fetched from source successfully!");
-        console.log(`Total rows fetched: ${data.length}`);
-  
-        // Close cursor and connection to source database
-        source_cursor.close();
-        source_conn.close();
-  
-        // Return fetched data
-        return data;
-      } catch (error) {
-        // Show error message
-        console.error("An error occurred while fetching data from source.");
-        console.error(error);
-      }
-    }
-  
-    insertDataIntoTarget(data) {
-      try {
-        // Connect to target database
-        const target_conn = this.connectToTarget();
-  
-        // Get cursor to target database
-        const target_cursor = target_conn.cursor();
-  
-        // Execute query to insert data into target
-        for (let i = 0; i < data.length; i++) {
-          target_cursor.execute(`INSERT INTO target_table VALUES (${data[i]})`);
-  
-          // Show progress update
-          console.log(`${i + 1} rows inserted into target.`);
-        }
-  
-        // Show success message
-        console.log("Data inserted into target successfully!");
-  
-        // Commit changes to target database
-        target_conn.commit();
-  
-        // Close cursor and connection to target database
-        target_cursor.close();
-        target_conn.close();
-      } catch (error) {
-        // Show error message
-        console.error("An error occurred while inserting data into target.");
-        console.error(error);
-      }
-    }
-  
-    // Method to connect to source database
-    connectToSource() {
-      // Code to connect to source database using source_conn_params
-    }
-  
-    // Method to connect to target database
-    connectToTarget() {
-      // Code to connect to target database using target_conn_params
+  constructor(source, target) {
+    this.source = source; 
+    this.target = target;
+  }
+
+  extract() {
+    switch(this.source.type){
+      case 'postgresql':
+        return this.extractFromPostgresql();
+      case 'mongodb':
+        return this.extractFromMongodb();
+      case 'oracle':
+        return this.extractFromOracle();
+      case 'sql_server':
+        return this.extractFromSqlServer();
+      default: 
+        throw new Error('Unsupported source database type: ${this.source.type}');
     }
   }
-  
+
+  extractFromPostgresql() {
+    // Implementation for extracting data from a PostgreSQL database
+  }
+
+  extractFromMongodb() {
+    // Implementation for extracting data from a MongoDB database
+  }
+
+  extractFromOracle() {
+    // Implementation for extracting data from an Oracle database
+  }
+
+  extractFromSqlServer() {
+    //Implementation for extracting data from a SQL Server database
+  }
+
+  transform(data) {
+    // Placeholder for any data transformations
+    return data;
+  }
+
+  load(data) {
+    switch (this.target.type) {
+      case 'postgresql': 
+        return this.loadIntoPostgresql(data);
+      case 'mongodb': 
+        return this.loadIntoMongoDb(data);
+      case 'oracle': 
+        return this.loadIntoOracle(data);
+      case 'sql_server':
+        return this.loadIntoSqlServer(data);
+      default: 
+        throw new Error('Unsupported target database type: ${this.target.type}');
+    }
+  }
+
+  loadIntoPostgresql(data) {
+    // Implementation for loading data into a PostgreSQL database
+  }
+
+  loadIntoMongoDb(data) {
+    // Implementation for loading data into a MongoDB database
+  }
+
+  loadIntoOracle(data) {
+    // Implementation for loading data into an Oracle database
+  }
+
+  loadIntoSqlServer(data) {
+    // Implementation for loading data into a SQL Server database
+  }
+
+  perform() {
+    const data = this.extract(); 
+    const transformedData = this.transform(data);
+    this.load(transformedData);
+  }
+}
+
+// Example usage: 
+const etl = new ETL({ type: 'postgresql'}, { type: 'oracle'});
+etl.perform();
