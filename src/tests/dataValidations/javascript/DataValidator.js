8bc7810990f3c14767fa92767cd61a0bc762089c
Joseph Woodall <josephlw4@gmail.com>
2023-02-13 12:33:09 -0600

    adds first pass at db adapters

--- src/tests/dataValidations/javascript/DataValidator.js
+++ src/tests/dataValidations/javascript/DataValidator.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const pg = require('pg');
-const mongoose = require('mongoose');
+const MongoClient = require('mongodb').MongoClient;
 const oracledb = require('oracledb');
 const tedious = require('tedious');
 const async = require('async');
@@ -85,58 +85,70 @@ class DbAdapter {
 class PostgresqlAdapter extends DbAdapter {
     async connect(config) {
         // connect to postgresql database
+        const client = new pg(config)
+        client.connect().then(() => {console.log("Connected to the Postgresql database")}).catch((err) => {console.log("Failed to connect to the Postgresql database:", err)});
     }
     async getData() {
         // get data from postgresql database
+        client.query("SELECT * FROM table_name", (err, res) => {
+            console.log(err ? err.stack:res.rows);
+        })
     }
     async close() {
         // close connection to the postgresql database
+        client.close().then(() => {console.log("Disconnected from the Postgresql database")}).catch((err) => {console.log("Failed to disconnect from the Postgreql database:", err)})
     }
 }
 
 class MongoDbAdapter extends DbAdapter {
     async connect(config) {
         // connect to the MongoDB database
+        const client = new MongoClient(config, { useNewUrlParser: true });
+        client.connect().then(() => {console.log("Connected to the Mongodb database")}).catch((err) => {console.log("Failed to connect to the Mongodb database:", err)});
+        const db = client.db('test');
     }
     async getData() {
         // get data from the mongodb database
+        const collection = db.collection("collection_name");
+        collection.find({}).toArray((err, users) => {console.log(users)});
     }
     async close() {
         // close connection to the mongodb database
+        client.close().then(() => {console.log("Disconnected from the Mongodb database")}).catch((err) => {console.log("Failed to disconnect from the Mongodb database:", err)})
+
     }
 }
 
 class OracleAdapter extends DbAdapter {
   async connect(config) {
-    // connect to Oracle database
-    // ...
+        const client = new oracledb.getConnection(config)
+        client.connect().then(() => {console.log("Connected to the Oracle database")}).catch((err) => {console.log("Failed to connect to the Oracle database:", err)});
   }
-
   async getData() {
-    // get data from Oracle database
-    // ...
+        // get data from Oracle database
+        client.execute("SELECT * FROM table_name")
   }
 
   async close() {
-    // close connection to Oracle database
-    // ...
+    client.close().then(() => {console.log("Disconnected from the Oracle database")}).catch((err) => {console.log("Failed to disconnect from the Oracle database:", err)})
   }
 }
 
 class SqlServerAdapter extends DbAdapter {
     async connect(config) {
-      // connect to SQL Server database
-      // ...
+      const client = new tedious.Connection(config)
+      client.connect().then(() => {console.log("Connected to the SQL Server database")}).catch((err) => {console.log("Failed to connect to the SQL Server database:", err)});
     }
   
     async getData() {
       // get data from SQL Server database
-      // ...
+      const request = new Request("SELECT * FROM table_name")
+      client.execSql(request);
     }
   
     async close() {
       // close connection to SQL Server database
-      // ...
+      client.close().then(() => {console.log("Disconnected from the SQL Server database")}).catch((err) => {console.log("Failed to disconnect from the SQL Server database:", err)})
     }
   }
 
