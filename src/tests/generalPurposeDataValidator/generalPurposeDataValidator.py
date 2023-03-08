d37a41f555d5dc90ca5a32ba6395549af82057ab
Joseph Woodall <56406362+RedLeadr@users.noreply.github.com>
2023-03-07 21:15:04 -0600

    adds general purpose data validator

--- src/tests/generalPurposeDataValidator/generalPurposeDataValidator.py
+++ src/tests/generalPurposeDataValidator/generalPurposeDataValidator.py
@@ -1,78 +1,116 @@
-import pandas as pd
-import sqlite3
+import csv
+import xlrd
 import psycopg2
+import cx_Oracle
 import pyodbc
-import unittest
+import os
 
+import unittest 
 
-class DataComparer:
-    def __init__(self, source=None, target=None):
-        self.source = source
-        self.target = target
+class DataReader:
+    def __init__(self, source_type=None, connection_string=None):
+        self.source_type = source_type
+        self.connection_string = connection_string
+        self.db_host = os.getenv("DB_HOST")
+        self.db_port = os.getenv("DB_PORT")
+        self.db_name = os.getenv("DB_NAME")
+        self.db_user = os.getenv("DB_USER")
+        self.db_password = os.getenv("DB_PASSWORD")
 
-    def load_data_from_csv(self, file_path):
-        return pd.read_csv(file_path)
+    def read_data(self, query):
+        if not self.source_type:
+            self.source_type = os.getenv("DB_SOURCE_TYPE")
 
-    def connect_to_database(self, db_type, db_name, host, port, user, password, table_name):
-        if db_type == 'sqlite':
-            conn = sqlite3.connect(db_name)
+        if not self.connection_string:
+            self.connection_string = f"host={self.db_host} port={self.db_port} dbname={self.db_name} user={self.db_user} password={self.db_password}"
+
+        if self.source_type.lower() == 'csv':
+            with open(self.connection_string, newline='') as csvfile:
+                reader = csv.reader(csvfile)
+                data = list(reader)
+        elif self.source_type.lower() == 'excel':
+            book = xlrd.open_workbook(self.connection_string)
+            sheet = book.sheet_by_index(0)
+            data = []
+            for row_idx in range(0, sheet.nrows):
+                row = []
+                for col_idx in range(0, sheet.ncols):
+                    cell_value = sheet.cell(row_idx, col_idx).value
+                    row.append(cell_value)
+                data.append(row)
+        elif self.source_type.lower() == 'postgresql':
+            conn = psycopg2.connect(self.connection_string)
             cursor = conn.cursor()
-            cursor.execute(f"SELECT * FROM {table_name}")
-            rows = cursor.fetchall()
-            columns = [description[0] for description in cursor.description]
-            return pd.DataFrame(rows, columns=columns)
-        elif db_type == 'postgres':
-            conn = psycopg2.connect(
-                host=host, port=port, user=user, password=password, database=db_name)
+            cursor.execute(query)
+            data = cursor.fetchall()
+            cursor.close()
+            conn.close()
+        elif self.source_type.lower() == 'oracle':
+            conn = cx_Oracle.connect(self.connection_string)
             cursor = conn.cursor()
-            cursor.execute(f"SELECT * FROM {table_name}")
-            rows = cursor.fetchall()
-            columns = [description[0] for description in cursor.description]
-            return pd.DataFrame(rows, columns=columns)
-        elif db_type == 'oracle':
-            dsn_tns = f"(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST={host})(PORT={port})))(CONNECT_DATA=(SID={db_name})))"
-            conn = cx_Oracle.connect(user=user, password=password, dsn=dsn_tns)
+            cursor.execute(query)
+            data = cursor.fetchall()
+            cursor.close()
+            conn.close()
+        elif self.source_type.lower() == 'sql server':
+            conn = pyodbc.connect(self.connection_string)
             cursor = conn.cursor()
-            cursor.execute(f"SELECT * FROM {table_name}")
-            rows = cursor.fetchall()
-            columns = [description[0] for description in cursor.description]
-            return pd.DataFrame(rows, columns=columns)
-        elif db_type == 'sqlserver':
-            conn = pyodbc.connect(
-                f"Driver={{SQL Server}};Server={host};Database={db_name};UID={user};PWD={password}")
-            cursor = conn.cursor()
-            cursor.execute(f"SELECT * FROM {table_name}")
-            rows = cursor.fetchall()
-            columns = [description[0] for description in cursor.description]
-            return pd.DataFrame(rows, columns=columns)
+            cursor.execute(query)
+            data = cursor.fetchall()
+            cursor.close()
+            conn.close()
         else:
-            raise ValueError(f"{db_type} is not a valid database type.")
+            raise ValueError('Invalid data source type')
+
+        return data
+
+class DataValidator(unittest.TestCase):
+    def __init__(self, data_objects):
+        super().__init__()
+        self.data_objects = data_objects
+        
+    def assert_data_equal(self):
+        for i in range(len(self.data_objects) -1):
+            for j in range(i + 1, len(self.data_objects)):
+                self.assertEqual(self.data_objects[i], self.data_objects[j])
+
+    def assert_column_names_equal(self):
+        column_names = [set(data.columns) for data in self.data_objects]
+        for i in range(len(column_names) - 1):
+            for j in range(i + 1, len(column_names)):
+                self.assertSetEqual(column_names[i], column_names[j])       
+
+'''
+Example Usage in a main.py file: 
+
+data_reader = DataReader()
+
+# Read data from two different tables with different column names
+query1 = "SELECT column1, column2 FROM table1"
+query2 = "SELECT column3, column4 FROM table2"
+data1 = data_reader.read_data(query1)
+data2 = data_reader.read_data(query2)
+validator = DataValidator([data1, data2])
+
+# Assert that the data objects are equal
+validator.assert_data_equal()
+
+# Assert that the column names are equal
+validator.assert_column_names_equal()
 
 
-class TestDataComparer(unittest.TestCase):
-    def setUp(self):
-        self.comparer = DataComparer(source={'database_name': 'database1', 'host': 'localhost', 'port': 5432, 'user': 'user1', 'password': 'password1', 'table_name': 'table1'},
-                                     target={'database_name': 'database2', 'host': 'localhost', '
-    def test_compare_data_when_source_data_is_none(self):
-        self.source_data=None
-        with self.assertRaises(Exception):
-            self.comparer.compare_data(self.source_data, self.target_data)
+# Read data from two different tables with same column names
+query3 = "SELECT column1, column2 FROM table3"
+query4 = "SELECT column1, column2 FROM table4"
+data3 = data_reader.read_data(query3)
+data4 = data_reader.read_data(query4)
+validator = DataValidator([data3, data4])
 
-    def test_compare_data_when_target_data_is_none(self):
-        self.target_data=None
-        with self.assertRaises(Exception):
-            self.comparer.compare_data(self.source_data, self.target_data)
+# Assert that the data objects are equal
+validator.assert_data_equal()
 
-    def test_compare_data_when_source_and_target_data_are_none(self):
-        self.source_data=None
-        self.target_data=None
-        with self.assertRaises(Exception):
-            self.comparer.compare_data(self.source_data, self.target_data)
+# Assert that the column names are equal
+validator.assert_column_names_equal()
 
-    def test_compare_data_when_columns_are_different(self):
-        self.source_data=pd.DataFrame(
-            {'id': [1, 2, 3], 'name': ['Alice', 'Bob', 'Charlie'], 'age': [25, 30, 40]})
-        self.target_data=pd.DataFrame({'id': [1, 2, 3], 'name': [
-                                      'Alice', 'Bob', 'Charlie'], 'address': ['NY', 'LA', 'SF']})
-        with self.assertRaises(Exception):
-            self.comparer.compare_data(self.source_data, self.target_data)
+
+'''
