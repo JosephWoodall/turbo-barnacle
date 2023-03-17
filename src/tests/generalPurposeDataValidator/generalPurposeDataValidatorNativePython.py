3d19b8db4c71a35fdc82a449268a88e93fd16d56
Joseph Woodall <56406362+RedLeadr@users.noreply.github.com>
2023-03-17 08:33:56 -0500

    changes native python validator

--- src/tests/generalPurposeDataValidator/generalPurposeDataValidatorNativePython.py
+++ src/tests/generalPurposeDataValidator/generalPurposeDataValidatorNativePython.py
@@ -9,57 +9,73 @@ class DataValidator:
         with psycopg2.connect(self.conn_str) as conn:
             with conn.cursor() as cur:
                 cur.execute(query)
-                data = cur.fetchall()
-                headers = [desc[0] for desc in cur.description]
-                return self.create_dict(headers, data)
+                rows = cur.fetchall()
+                columns = [desc[0] for desc in cur.description]
+                data = [dict(zip(columns, row)) for row in rows]
+        return data
 
     def read_csv(self, file_path):
-        with open(file_path, 'r') as f:
-            reader = csv.reader(f)
-            headers = next(reader)
-            data = list(reader)
-            return self.create_dict(headers, data)
-
-    def create_dict(self, headers, data):
-        data_dict = []
-        for row in data:
-            row_dict = {}
-            for i, h in enumerate(headers):
-                row_dict[h] = row[i]
-            data_dict.append(row_dict)
-        return data_dict
+        with open(file_path, "r") as f:
+            reader = csv.DictReader(f)
+            data = [row for row in reader]
+        return data
 
     def validate_column(self, col_name, data1, data2):
-        is_equal = [d1[col_name] == d2[col_name] for d1, d2 in zip(data1, data2)]
+        is_equal = [row1[col_name] == row2[col_name] for row1, row2 in zip(data1, data2)]
         return is_equal
 
     def validate_datasets(self, common_col, data1, data2):
         # check if column is present in both datasets
-        if common_col not in data1[0] or common_col not in data2[0]:
+        if common_col not in data1[0].keys() or common_col not in data2[0].keys():
             raise ValueError(f"Column '{common_col}' not found in both datasets.")
 
         # check if common column values match
         is_equal = self.validate_column(common_col, data1, data2)
         if not all(is_equal):
             raise ValueError(f"Data in column '{common_col}' does not match between datasets.")
-        
-        # additional validation tests can be added here
-        # ...
+
+        # group the data by the common column
+        groups1 = {}
+        for row in data1:
+            key = row[common_col]
+            if key in groups1:
+                groups1[key].append(row)
+            else:
+                groups1[key] = [row]
+        groups2 = {}
+        for row in data2:
+            key = row[common_col]
+            if key in groups2:
+                groups2[key].append(row)
+            else:
+                groups2[key] = [row]
+
+        # calculate the difference between the financial columns of your choice (revenue, margin, and cost columns, for example)
+        diff_dict = {}
+        for key in groups1.keys() & groups2.keys():
+            diff_dict[key] = {col: sum(row[col] for row in groups1[key]) - sum(row[col] for row in groups2[key]) for col in ['FINANCIAL_COLUMN_1', 'FINANCIAL_COLUMN_2', 'FINANCIAL_COLUMN_3']}
+
+        # display the difference between the financial columns of your choice (revenue, margin, and cost columns for each common customer for example)
+        print(diff_dict)
 
         return True
 
+
 '''
-Example Usage:
+Example Usage 
 
-# create DataValidator object with database connection string
-validator = DataValidator('dbname=mydb user=myuser password=mypass host=localhost port=5432')
+# fetching data from a database
+conn_str = "postgresql://user:password@host:port/database"
+validator = DataValidator(conn_str)
 
-# fetch data from database
-data1 = validator.fetch_data('SELECT * FROM table1')
-data2 = validator.fetch_data('SELECT * FROM table2')
+query = "SELECT * FROM my_table"
+data1 = validator.fetch_data(query)
 
-# read CSV files
-data1 = validator.read_csv('file1.csv')
-data2 = validator.read_csv('file2.csv')
+# fetching data from a csv file
+file_path = "my_file.csv"
+data2 = validator.read_csv(file_path)
 
+# specify the common column
+common_col = "my_common_column"
+is_valid = validator.validate_datasets(common_col, data1, data2)
 '''
