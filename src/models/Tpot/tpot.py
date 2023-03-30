188276dd8564d0a8729218d361daeb226d08cb7d
Joseph Woodall <josephlw4@gmail.com>
2023-03-30 11:44:23 -0500

    adds dockerfile and requirements, fix error in tpo

--- src/models/Tpot/tpot.py
+++ src/models/Tpot/tpot.py
@@ -1,4 +1,4 @@
-import pandas as pd 
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from tpot import TPOTClassifier
 
@@ -8,14 +8,14 @@ SCORE = ''
 df = pd.read_csv(PATH)
 
 y = df['target']
-x = df.drop('target', axis = 1)
+x = df.drop('target', axis=1)
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.1)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1)
 
-tpot = TPOTClassifier(scoring = SCORE, verbosity = 2)
+tpot = TPOTClassifier(scoring=SCORE, verbosity=2)
 
 tpot.fit(x_train, y_train)
 
 print(tpot.score(x_test, y_test))
 
-pot.export('best_classifier_model.py')
+tpot.export('best_classifier_model.py')
