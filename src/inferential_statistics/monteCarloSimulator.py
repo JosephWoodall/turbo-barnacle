502042a4569ed9badb8c6a09c832418d9d151112
Joseph Woodall <josephlw4@gmail.com>
2023-03-31 16:24:04 -0500

    adds update to mcs

--- src/inferential_statistics/monteCarloSimulator.py
+++ src/inferential_statistics/monteCarloSimulator.py
@@ -1,77 +1,97 @@
 import polars as pl
 import numpy as np
+from typing import List, Dict
 
 
 class MonteCarloSimulator:
-    """
-    A Monte Carlo simulator for inferential statistics.
-    """
+    def __init__(self, data: pl.DataFrame):
+        self.data = data
 
-    def __init__(self, sample_size, null_mean, null_std, alternative_mean, alternative_std, alpha, num_simulations):
-        """
-        Initializes the MonteCarloSimulator object.
+    def simulate(self, func: str, n_simulations: int, random_state: int = 42, **kwargs) -> List[float]:
+        """Run a Monte Carlo simulation on the given function using the data provided.
 
         Args:
-            sample_size (int): The sample size.
-            null_mean (float): The null hypothesis mean.
-            null_std (float): The null hypothesis standard deviation.
-            alternative_mean (float): The alternative hypothesis mean.
-            alternative_std (float): The alternative hypothesis standard deviation.
-            alpha (float): The significance level.
-            num_simulations (int): The number of simulations to run.
+            func (str): The name of the function to simulate.
+            n_simulations (int): The number of simulations to run.
+            random_state (int): The random seed to use for the simulation.
+            kwargs: Additional keyword arguments to pass to the function.
+
+        Returns:
+            List[float]: A list of the results from each simulation.
         """
-        self.sample_size = sample_size
-        self.null_mean = null_mean
-        self.null_std = null_std
-        self.alternative_mean = alternative_mean
-        self.alternative_std = alternative_std
-        self.alpha = alpha
-        self.num_simulations = num_simulations
+        # Set the random seed
+        np.random.seed(random_state)
 
-    def run_simulation(self):
+        # Create an empty list to store the simulation results
+        simulation_results = []
+
+        # Loop through each simulation
+        for i in range(n_simulations):
+            # Sample data from the dataframe with replacement
+            sampled_data = self.data.sample_n(len(self.data), replace=True)
+
+            # Call the function with the sampled data and any additional keyword arguments
+            result = getattr(sampled_data, func)(**kwargs)
+
+            # Append the result to the simulation results
+            simulation_results.append(result)
+
+        return simulation_results
+
+    def infer(self, simulation_results: List[float], alpha: float = 0.05) -> Dict[str, float]:
+        """Perform statistical inference on the given simulation results.
+
+        Args:
+            simulation_results (List[float]): The results from the Monte Carlo simulation.
+            alpha (float): The significance level to use for the hypothesis test.
+
+        Returns:
+            Dict[str, float]: A dictionary containing the inferential statistics.
         """
-        Runs the Monte Carlo simulation and returns the p-value.
-        """
-        # Generate a normal distribution with the null hypothesis parameters
-        null_distribution = pl.Series(np.random.normal(
-            self.null_mean, self.null_std, self.sample_size * self.num_simulations))
+        # Calculate the mean and standard deviation of the simulation results
+        mean = np.mean(simulation_results)
+        std_dev = np.std(simulation_results)
 
-        # Generate a normal distribution with the alternative hypothesis parameters
-        alternative_distribution = pl.Series(np.random.normal(
-            self.alternative_mean, self.alternative_std, self.sample_size * self.num_simulations))
+        # Calculate the confidence interval
+        lower_bound, upper_bound = np.percentile(
+            simulation_results, [100 * alpha / 2, 100 * (1 - alpha / 2)])
 
-        # Calculate the t-statistic for each sample
-        null_sample_means = null_distribution.reshape(
-            self.num_simulations, self.sample_size).mean(axis=1)
-        alternative_sample_means = alternative_distribution.reshape(
-            self.num_simulations, self.sample_size).mean(axis=1)
-        t_values = (alternative_sample_means - self.null_mean) / \
-            (self.null_std / np.sqrt(self.sample_size))
+        # Perform a hypothesis test
+        null_hypothesis = 0  # Set the null hypothesis to zero
+        p_value = 2 * min(np.mean(simulation_results <= null_hypothesis),
+                          np.mean(simulation_results >= null_hypothesis))
 
-        # Calculate the p-value
-        if self.alternative_mean > self.null_mean:
-            p_value = (t_values > pl.Series(self.alpha)
-                       ).sum() / self.num_simulations
-        else:
-            p_value = (t_values < pl.Series(-self.alpha)
-                       ).sum() / self.num_simulations
+        # Create a dictionary of the inferential statistics
+        inferential_stats = {
+            "mean": mean,
+            "std_dev": std_dev,
+            "lower_bound": lower_bound,
+            "upper_bound": upper_bound,
+            "p_value": p_value
+        }
 
-        return p_value
+        return inferential_stats
+
+# Example usage
 
 
-# Initialize the MonteCarloSimulator object
-simulator = MonteCarloSimulator(
-    sample_size=10,
-    null_mean=100,
-    null_std=10,
-    alternative_mean=105,
-    alternative_std=10,
-    alpha=0.05,
-    num_simulations=1000
-)
+# Load new inference data into Polars DataFrame
+data = pl.DataFrame({
+    'x1': [1, 2, 3, 4, 5],
+    'x2': [6, 7, 8, 9, 10],
+    'y': [10, 20, 30, 40, 50]
+})
 
-# Run the simulation
-p_value = simulator.run_simulation()
+# Instantiate MonteCarlo class
+mc = MonteCarloSimulator(data=data, response='y', n_simulations=1000)
 
-# Print the result
-print(f"p-value: {p_value}")
+# Run Monte Carlo simulation
+mc.run()
+
+# Calculate mean and confidence intervals for each explanatory variable
+means, ci_low, ci_high = mc.infer_means_ci()
+
+# Print results
+for col, mean, low, high in zip(data.columns[:-1], means, ci_low, ci_high):
+    print(
+        f"Variable: {col}, Mean: {mean:.2f}, 95% CI: ({low:.2f}, {high:.2f})")
