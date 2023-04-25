917b47faf875025dffa11a2a19386fac86ee9eb1
Joseph Woodall <josephlw4@gmail.com>
2023-04-25 14:32:00 -0500

    adds more inspection classes

--- src/ml_system_boilerplate_code/metaflow/utils/inspection.py
+++ src/ml_system_boilerplate_code/metaflow/utils/inspection.py
@@ -1,8 +1,19 @@
 class PastRunInspection:
+    """
+    The PastRunInspection will return the last successful run for the user's namespace.
+    """
     from metaflow import Flow
     run = Flow('Main').latest_successful_run
     print(run)
 
 
+class PastRunStartCardInspection:
+    """
+    The PastRunStartCardInspection will return the card inspection for the last successful run for the user's namespace.
+    """
+    import os
+    os.system('python3 main.py card view start')
+
+
 if __name__ == "__main__":
-    PastRunInspection()
+    PastRunStartCardInspection()
