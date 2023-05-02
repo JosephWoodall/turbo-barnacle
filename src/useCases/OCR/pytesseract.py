08c6ff0bbd04ed6fb811887fe239d7c076d54d49
Joseph Woodall <josephlw4@gmail.com>
2023-05-02 09:52:49 -0500

    corrects ocr

--- src/useCases/OCR/pytesseract.py
+++ src/useCases/OCR/pytesseract.py
@@ -6,10 +6,10 @@ pdf_file = 'example.pdf'
 
 # Open the PDF file using PyPDF2
 with open(pdf_file, 'rb') as file:
-    pdf_reader = PyPDF2.PdfFileReader(file)
+    pdf_reader = PyPDF2.PdfReader(file)
 
     # Loop through each page of the PDF file and extract the text using Tesseract
-    for page_num in range(pdf_reader.getNumPages()):
-        page = pdf_reader.getPage(page_num)
-        text = page.extractText()
+    for page_num in range(len(pdf_reader.pages)):
+        page = pdf_reader.pages[page_num]
+        text = page.extract_text()
         print(text)
